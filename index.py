fc9db1b98a76c03854357772edd6d54b58c76dfa
ZCosmicc <farizfadillah612@gmail.com>
2024-11-01 23:18:54 +0800

    initial

---
+++ index.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8-CBAM.yaml")  # build a new model from scratch
+
+# Use the model
+model.train(data="data.yaml", epochs=10)  # train the model
+metrics = model.val()  # evaluate model performance on the validation set
